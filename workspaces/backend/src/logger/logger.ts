21664610fb9a4c7a1b8733c47e7cb5cae90d151c
Josemi Chaves <josemichaves@gmail.com>
2021-06-21 19:37:34 +0200

    changes

---
+++ workspaces/backend/src/logger/logger.ts
@@ -0,0 +1,6 @@
+import winston from 'winston';
+export const logger = winston.createLogger({
+  level: 'silly',
+  format: winston.format.simple(),
+  transports: [new winston.transports.Console()],
+});
