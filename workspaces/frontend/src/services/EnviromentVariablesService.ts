a384a94bff69e2bc58cf43f0273279736394f378
Josemi Chaves <josemichaves@gmail.com>
2021-07-26 22:02:13 +0200

    Changes in env to deploy easier

--- workspaces/frontend/src/services/EnviromentVariablesService.ts
+++ workspaces/frontend/src/services/EnviromentVariablesService.ts
@@ -1,15 +1,26 @@
 require('dotenv').config();
-
 export class EnviromentVariables {
   static getUriForHttpServer() {
-    return `http://${process.env.REACT_APP_HTTP_SERVER_HOST}:${this.getPort()}`;
+    if (process.env.REACT_APP_ENVIROMENT === 'dev') {
+      return `http://localhost:${this.getPort()}`;
+    } else {
+      return `http://${
+        process.env.REACT_APP_SERVER_URI
+      }:${EnviromentVariables.getPort()}`;
+    }
   }
   static getUriForWsServer() {
-    return (
-      `ws://${process.env.REACT_APP_WS_SERVER_HOST}:${this.getPort()}/${
+    if (process.env.REACT_APP_ENVIROMENT === 'dev') {
+      return `ws://localhost:${EnviromentVariables.getPort()}/${
         process.env.REACT_APP_WS_SERVER_PATH
-      }` || 'ws://localhost:4000/projects'
-    );
+      }`;
+    } else {
+      return `ws://${
+        process.env.REACT_APP_SERVER_URI
+      }:${EnviromentVariables.getPort()}/${
+        process.env.REACT_APP_WS_SERVER_PATH
+      }`;
+    }
   }
   static getPort() {
     return process.env.REACT_APP_SERVER_PORT || 4000;
